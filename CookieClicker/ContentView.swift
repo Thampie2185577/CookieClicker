47ef304ebb37bf4b9c639e8e6e15f3622db821fd
Thampie <thampie55@gmail.com>
2025-02-07 10:52:38 +0100

    clickable cookie

--- CookieClicker/ContentView.swift
+++ CookieClicker/ContentView.swift
@@ -1,21 +1,32 @@
-//
-//  ContentView.swift
-//  CookieClicker
-//
-//  Created by Thampie Anthonyrajah on 07/02/2025.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State var timesClicked = 0;
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, !")
-        }
-        .padding()
+        ZStack {
+            VStack {
+                NavigationView {
+                }.navigationViewStyle(StackNavigationViewStyle()).navigationBarTitle("CookieClicker")
+                Button(
+                    action: {
+                        timesClicked += 1
+                        print(timesClicked)
+                    },
+                    label: {
+                    Image(.cookie).resizable().aspectRatio(contentMode: .fit)
+                    })
+                
+                if(timesClicked == 0){
+                    Text("Click the cookie!").padding(.top, 20)
+                }
+                else {
+                    Text(String(timesClicked)).padding(.top, 20)
+                }
+                Spacer()
+                
+            }
+        }.background(Color.gray).ignoresSafeArea()
     }
 }
 
