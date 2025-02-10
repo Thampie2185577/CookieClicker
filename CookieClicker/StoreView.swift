0c9e9ca038c417a0bb465b699c0f5d08fca32991
Thampie <thampie55@gmail.com>
2025-02-10 12:00:48 +0100

    added store function

--- CookieClicker/StoreView.swift
+++ CookieClicker/StoreView.swift
@@ -14,10 +14,35 @@ struct StoreView: View {
         VStack{
             Text("Store")
             Spacer()
-            Text("\(timesClicked)")
+            Text("Koop goodies van je cookies")
+            Text("tegoed: \(timesClicked) cookies")
+            returnButtons().padding(20)
+            Spacer()
+            Spacer()
         }
     }
+    
+    func returnButtons() -> some View{
+        return VStack{
+            Button("Koop een grandma(15 cookies)") {
+                timesClicked -= 15;
+            }
+            Button("Koop een robot(100 cookies)") {
+                print("Koop")
+            }
+            Button("Koop een farm(cookies)") {
+                print("Welkom")
+            }
+            Button("Koop een mine(1000 cookies)") {
+                print("Welkom")
+            }
+        }
+        
+    }
 }
+
+
+
 
 #Preview {
     StoreView(timesClicked: .constant(0))
